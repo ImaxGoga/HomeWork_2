3795ae115a8691e6d9bee51b862e0fa34698e3d7
ImaxGoga <45959194+ImaxGoga@users.noreply.github.com>
2022-08-15 13:06:15 +0300

    third request done

---
+++ 2_3/Program.cs
@@ -0,0 +1,21 @@
+﻿//Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+int UserDay;
+do {
+Console.Write("Введите порядок дня недели: ");
+UserDay = Convert.ToInt32(Console.ReadLine());
+if (UserDay > 7)
+{
+    Console.WriteLine("Ошибка! День недели может быть от 1 до 7(включительно)!");
+}
+}
+while(UserDay > 7 );
+
+if(UserDay >= 6)
+{
+    Console.Write("День " + UserDay + " является выходным!");
+}
+else {
+    Console.Write("День " + UserDay + " не является выходным!");
+    }
+
