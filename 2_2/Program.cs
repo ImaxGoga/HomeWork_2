af58a3781088e13929484d4ebbe9b7d147b3e990
ImaxGoga <45959194+ImaxGoga@users.noreply.github.com>
2022-08-15 12:52:53 +0300

    Second request done

---
+++ 2_2/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите число: ");
+string a1 = Console.ReadLine();
+
+if(a1.Length >= 3)
+{
+    Console.Write("Третья цифра: ");
+    Console.Write(a1[2]);
+}
+else {
+    Console.Write("Третьей цифры нет");
+}
